f2495497234ad44e90baf9afd77df5aba287511b
Sergey Kykosh <adm1n.bmz@gmail.com>
2015-01-19 11:45:16 +0200

    First commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '077869258',
+    'charset' => 'utf8',
+];
---
+++ views/site/entry.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use \yii\bootstrap\DatePicker;
+?>
+<?php $form = ActiveForm::begin();?>
+<?= $form->field($model, 'name') ?>
+
+<?= $form->field($model, 'email') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+<?php ActiveForm::end(); ?>
+<?= DatePicker::widget(['name' => 'date']) ?>
