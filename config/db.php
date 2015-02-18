9b6658bc622ab1837b09c283f34351c50b223acf
Sergey <adm1n.bmz@gmail.com>
2015-02-18 09:35:35 +0200

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=db_bmz',
-    'username' => 'bmz',
-    'password' => '077869258',
-    'charset' => 'utf8',
-];
