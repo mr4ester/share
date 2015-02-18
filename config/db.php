3e941857e7621d61080f2f3f26b584d7cc990f77
Sergey Kykosh <adm1n.bmz@gmail.com>
2015-02-18 09:35:59 +0200

    new passwd

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;port=5432;dbname=db_bmz',
     'username' => 'bmz',
-    'password' => '077869258',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
