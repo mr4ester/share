1c110a006a796aca06a9ff51a0e108225f418381
Kykosh Sergey <adm1n.bmz@gmail.com>
2015-03-02 14:36:05 +0200

    Дописаны методы добавления мониторов принтеров и конфигурации

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -19,8 +19,6 @@ use yii\web\Response;
 use yii\helpers\ArrayHelper;
 
 
-
-
 class UploadController extends Controller
 {
     public $data;
@@ -142,11 +140,12 @@ class UploadController extends Controller
 
     }
 
-    public function actionMonitors(){
+    public function actionMonitors()
+    {
         $model = new Monitors();
         $session = new Session();
 
-        if ($array=Yii::$app->request->post()) {
+        if ($array = Yii::$app->request->post()) {
             $id_staff = ArrayHelper::remove($array['Monitors'], 'staff');
             $model->load($array);
             $model->save();
@@ -154,55 +153,56 @@ class UploadController extends Controller
             $updateStaff = Staff::findOne($id_staff); // делаем запрос к таблице сотрудники, с id выбранным в форме
             $updateStaff->id_monitor = $key;// добавляем id конфигурации к выбранному сотруднику
             $updateStaff->save(); // и обновляем запись в базе данных
-            return $this->redirect(['upload/configuration' ,'id'=> $id_staff]);
+            return $this->redirect(['upload/configuration', 'id' => $id_staff]);
         }
 
         $staff = Staff::find()->all();
-        $listData = ArrayHelper::map($staff,'id_staff', 'fio' );// выбирает из масиива ключ-значение
+        $listData = ArrayHelper::map($staff, 'id_staff', 'fio');// выбирает из масиива ключ-значение
 
         $model->attributes = [      //заполняем атрибуты модели данными из массива
 
-                'id_monitor' => '',
-                'invent_num_monitor_1' =>'' ,
-                'invent_num_monitor_2' => '',
-                'monitor_1' => $session['config']['монитор1'],
-                'monitor_2' => $session['config']['монитор2'],
-                'date_1' => '',
-                'date_2' => '',
-                'old_staff_1' => '{0}',
-                'old_staff_2' => '{0}',];
+            'id_monitor' => '',
+            'invent_num_monitor_1' => '',
+            'invent_num_monitor_2' => '',
+            'monitor_1' => $session['config']['монитор1'],
+            'monitor_2' => $session['config']['монитор2'],
+            'date_1' => '',
+            'date_2' => '',
+            'old_staff_1' => '{0}',
+            'old_staff_2' => '{0}',];
 
-        return $this->render('/monitors/create',[
+        return $this->render('/monitors/create', [
             'model' => $model,
-            'listData'=> $listData,
-    ]);
+            'listData' => $listData,
+        ]);
 
     }
 
 
-    public function actionConfiguration(){
+    public function actionConfiguration()
+    {
         $model = new Configuration();
         $session = new Session();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()){
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
             $key = $model->getPrimaryKey(); //получаем последний id конфигурации
             $updateStaff = Staff::findOne(Yii::$app->request->get('id')); // делаем запрос к таблице сотрудники, с id выбранным в форме
             $updateStaff->id_configuration = $key;// добавляем id конфигурации к выбранному сотруднику
             $updateStaff->save(); // и обновляем запись в базе данных
-            return $this->redirect(['upload/printers' ,'id'=> Yii::$app->request->get('id')]);
+            return $this->redirect(['upload/printers', 'id' => Yii::$app->request->get('id')]);
         }
 
         /* переберем массив с RAM чтоб задать полям значения по дефолту если планка отсутсвует */
-        $ram = [1=>'',2=>'',3=>'', 4=>'']; // массив для хранения
+        $ram = [1 => '', 2 => '', 3 => '', 4 => '']; // массив для хранения
 
-        foreach($session['memory'] as $key =>$value){ //перебираем основной массив $key = 'имя модуля' $value = [модуль1],[модуль2]...
+        foreach ($session['memory'] as $key => $value) { //перебираем основной массив $key = 'имя модуля' $value = [модуль1],[модуль2]...
             $count = 0;                                 //счетчик
-           foreach($value as $key2 => $value2){  // перебираем значения
-               $count++;
-               if ($value2 !== ''){   // если мадуль1, модуль2 не пустой, присваеваем значение. И так в цикле получаем
-                   $ram[$count] .= '('. $value2 . ')' . ' ' ; // строку вида (Kingston 99U5474-013.A00LF) (2 Гб (1 rank, 8 banks)) (DDR3-1333 (667 МГц))
-               }
-           }
+            foreach ($value as $key2 => $value2) {  // перебираем значения
+                $count++;
+                if ($value2 !== '') {   // если мадуль1, модуль2 не пустой, присваеваем значение. И так в цикле получаем
+                    $ram[$count] .= '(' . $value2 . ')' . ' '; // строку вида (Kingston 99U5474-013.A00LF) (2 Гб (1 rank, 8 banks)) (DDR3-1333 (667 МГц))
+                }
+            }
         }
 
         $model->attributes = [                  //заполняем модель данными
@@ -212,23 +212,24 @@ class UploadController extends Controller
             'hdd_1' => $session['config']['дисковый накопитель1'],
             'hdd_2' => $session['config']['дисковый накопитель2'],
             'memory_1' => $ram[1],
-            'memory_2' =>  $ram[2],
+            'memory_2' => $ram[2],
             'memory_3' => $ram[3],
             'memory_4' => $ram[4],
             'mac' => $session['config']['первичный адрес mac'],
             'date' => '',
         ];
-                return $this->render('/configuration/create',[
-            'model'=>$model,
+        return $this->render('/configuration/create', [
+            'model' => $model,
         ]);
     }
 
 
-    public function actionPrinters(){
+    public function actionPrinters()
+    {
         $model = new Printers();
         $session = new Session();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()){
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
             $key = $model->getPrimaryKey(); //получаем последний id конфигурации
             $updateStaff = Staff::findOne(Yii::$app->request->get('id')); // делаем запрос к таблице сотрудники, с id выбранным в форме
             $updateStaff->id_printer = $key;// добавляем id конфигурации к выбранному сотруднику
@@ -237,7 +238,7 @@ class UploadController extends Controller
         }
 
 
-        $model->attributes=[
+        $model->attributes = [
             'print_1' => $session['config']['принтер1'],
             'print_2' => $session['config']['принтер2'],
             'print_3' => $session['config']['принтер3'],
@@ -250,11 +251,10 @@ class UploadController extends Controller
             'print_10' => $session['config']['принтер10'],
         ];
 
-        return $this->render('/printers/create',[
-            'model'=>$model
+        return $this->render('/printers/create', [
+            'model' => $model
         ]);
     }
-
 
 
     public function actionValidate()
@@ -266,14 +266,14 @@ class UploadController extends Controller
 
 
         $staff = Staff::find()->all();
-        $listData = ArrayHelper::map($staff,'id_staff', 'fio' );// выбирает из масиива ключ-значение
+        $listData = ArrayHelper::map($staff, 'id_staff', 'fio');// выбирает из масиива ключ-значение
 
 
         if (Yii::$app->request->post('ValidateForm')) { // если была отправленна форма
             $array = Yii::$app->request->post('ValidateForm');
             $saveConf->date = $array['date'];
             $saveConf->invent_num_system = $array['inv_syst'];
-            $saveConf->invent_num_monitor 	 = $array['inv_mon'];
+            $saveConf->invent_num_monitor = $array['inv_mon'];
             $saveConf->invent_num_printer = $array['invent_print'];
             $saveConf->cpu = $array['cpu'];
             $saveConf->motherboard = $array['motherboard'];
@@ -321,8 +321,8 @@ class UploadController extends Controller
                 $saveConf->print_9 = $array['printer9'];
             }
             if (isset($array['printer10'])) {
-            $saveConf->print_10 = $array['printer10'];
-             }
+                $saveConf->print_10 = $array['printer10'];
+            }
             $saveConf->save(); //сохраняем конфигурацию
             $key = $saveConf->getPrimaryKey(); //получаем последний id конфигурации
             $updateStaff = Staff::findOne($array['staff']); // делаем запрос к таблице сотрудники, с id выбранным в форме
@@ -340,8 +340,8 @@ class UploadController extends Controller
         return $this->render('validate', [
             'model' => $model,
             'config' => $session['config'],
-            'memory'=> $session['memory'],
-            'listData'=> $listData
+            'memory' => $session['memory'],
+            'listData' => $listData
         ]);
 
     }
