093ecaaeae5f23a5b844c9858d8b498e1020537d
Kykosh Sergey <adm1n.bmz@gmail.com>
2015-03-16 12:49:22 +0200

    перезаливка после обновления yii2

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=db_bmz',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=bmz',
     'username' => 'bmz',
     'password' => 'root',
     'charset' => 'utf8',
